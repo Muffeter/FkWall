44d9885047a76f14eeb3f136eaf569c99a13baec
Muffeter <sonmu.def@gmail.com>
2024-05-20 20:02:19 +0800

    apt pip conda added

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#更新apt源为清华源
+set -eux
+echo "[Info] 正在备份默认apt源..."
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "[Info] 正在替换apt源为清华apt源..."
+cat << EOF > /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+EOF
+echo "[Info] 正在更新源..."
+apt update
+echo "[Info] 正在更新软件..."
+apt upgrade -y
+
+#更新pip源为清华源
+if command -v pip &> /dev/null
+then
+	pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+fi
+
+#更新conda源为清华源
+if command -v conda &> /dev/null
+then
+	cat << EOF > ~/.condarc
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  deepmodeling: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/
+	EOF
+	conda clean -i
+	
+fi
+
